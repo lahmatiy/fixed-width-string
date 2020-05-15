384d428ab3ac177900f54cd66110bcd33a445351
Roman Dvornov <rdvornov@gmail.com>
2020-05-15 10:56:17 +0200

    Bump deps, add eslint & coverage, fix codestyle

--- index.js
+++ index.js
@@ -1,114 +1,109 @@
-var ANSI_REGEXP = /([\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><])/g;
+const ANSI_REGEXP = /([\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><])/g;
 
-function repeatStr(str, len){
-  return new Array(parseInt(len) + 1).join(str);
+function repeatStr(str, len) {
+    return new Array(parseInt(len) + 1).join(str);
 }
 
-function stripAnsi(str){
-  return str.replace(ANSI_REGEXP, '');
+function stripAnsi(str) {
+    return str.replace(ANSI_REGEXP, '');
 }
 
-module.exports = function fixedWidthString(str, width, options){
-  str = String(str);
-  width = parseInt(width, 10) || 0;
-  options = options || {};
+module.exports = function fixedWidthString(str, width, options) {
+    str = String(str);
+    width = parseInt(width, 10) || 0;
+    options = options || {};
 
-  var strWoAnsi = stripAnsi(str);
+    const strWoAnsi = stripAnsi(str);
 
-  // return empty string for bad width
-  if (width <= 0) {
-    return '';
-  }
-
-  // do nothing
-  if (strWoAnsi.length == width) {
-    return str;
-  }
-
-  // padding
-  if (strWoAnsi.length <= width) {
-    var paddingWidth = Math.max(width - strWoAnsi.length, 0);
-    var paddingString = options.padding ? fixedWidthString(options.padding, 1, { ellipsis: '' }) : ' ';
-    var padding = repeatStr(paddingString, paddingWidth);
-
-    if (options.align == 'right')
-      return padding + str;
-    else
-      return str + padding;
-  }
-
-  // truncating
-
-  // try to be intuitive
-  // if align to right and truncate doesn't specify – truncate from left
-  if (options.align == 'right' && 'truncate' in options == false) {
-    options.truncate = 'left';
-  }
-
-  var parts = str.split(ANSI_REGEXP);
-  var ellipsis = typeof options.ellipsis === 'string'
-    ? options.ellipsis
-    : ('ellipsis' in options === false || options.ellipsis ? '\u2026' : '');  // '\u2026' === '…'
-  var ellipsisWoAnsi = stripAnsi(ellipsis);
-  var result = '';
-
-  if (ellipsisWoAnsi.length >= width) {
-    return fixedWidthString(ellipsis, width, { ellipsis: '' });
-  }
-
-  width -= ellipsis.length;
-
-  if (options.truncate == 'left') {
-    // trancate left
-    // 'very long str' -> '…str'
-    for (var i = parts.length - 1, len = 0; i >= 0; i--) {
-      var part = parts[i];
-
-      // process non-ansi parts only
-      if (i % 2 == 0) {
-        if (len + part.length >= width) {
-          result =
-            // ignore all other plain strings left
-            parts.slice(0, i).filter(function(str, idx){
-              return idx % 2 ? str : '';
-            }).join('') +
-            ellipsis + part.substr(part.length - (width - len)) +
-            result;
-
-          break;
-        }
-
-        len += part.length;
-      }
-
-      result = part + result;
+    // return empty string for bad width
+    if (width <= 0) {
+        return '';
     }
-  } else {
-    // trancate right
-    // 'very long str' -> 'ver…'
-    for (var i = 0, len = 0; i < parts.length; i++) {
-      var part = parts[i];
 
-      // process non-ansi parts only
-      if (i % 2 == 0) {
-        if (len + part.length >= width) {
-          result =
-            result +
-            part.substr(0, width - len) + ellipsis +
-            // ignore all other plain strings
-            parts.slice(i).filter(function(str, idx){
-              return idx % 2 ? str : '';
-            }).join('');
-
-          break;
-        }
-
-        len += part.length;
-      }
-
-      result += part;
+    // do nothing
+    if (strWoAnsi.length == width) {
+        return str;
     }
-  }
 
-  return result;
+    // padding
+    if (strWoAnsi.length <= width) {
+        const paddingWidth = Math.max(width - strWoAnsi.length, 0);
+        const paddingString = options.padding ? fixedWidthString(options.padding, 1, { ellipsis: '' }) : ' ';
+        const padding = repeatStr(paddingString, paddingWidth);
+
+        return options.align == 'right'
+            ? padding + str
+            : str + padding;
+    }
+
+    // truncating
+
+    // try to be intuitive
+    // if align to right and truncate doesn't specify – truncate from left
+    if (options.align == 'right' && 'truncate' in options == false) {
+        options.truncate = 'left';
+    }
+
+    const parts = str.split(ANSI_REGEXP);
+    const ellipsis = typeof options.ellipsis === 'string'
+        ? options.ellipsis
+        : ('ellipsis' in options === false || options.ellipsis ? '\u2026' : '');  // '\u2026' === '…'
+    const ellipsisWoAnsi = stripAnsi(ellipsis);
+    let result = '';
+
+    if (ellipsisWoAnsi.length >= width) {
+        return fixedWidthString(ellipsis, width, { ellipsis: '' });
+    }
+
+    width -= ellipsis.length;
+
+    if (options.truncate == 'left') {
+        // trancate left
+        // 'very long str' -> '…str'
+        for (let i = parts.length - 1, len = 0; i >= 0; i--) {
+            const part = parts[i];
+
+            // process non-ansi parts only
+            if (i % 2 == 0) {
+                if (len + part.length >= width) {
+                    result =
+                        // ignore all other plain strings left
+                        parts.slice(0, i).filter((str, idx) => idx % 2 ? str : '').join('') +
+                        ellipsis + part.substr(part.length - (width - len)) +
+                        result;
+
+                    break;
+                }
+
+                len += part.length;
+            }
+
+            result = part + result;
+        }
+    } else {
+        // trancate right
+        // 'very long str' -> 'ver…'
+        for (let i = 0, len = 0; i < parts.length; i++) {
+            const part = parts[i];
+
+            // process non-ansi parts only
+            if (i % 2 == 0) {
+                if (len + part.length >= width) {
+                    result =
+                        result +
+                        part.substr(0, width - len) + ellipsis +
+                        // ignore all other plain strings
+                        parts.slice(i).filter((str, idx) => idx % 2 ? str : '').join('');
+
+                    break;
+                }
+
+                len += part.length;
+            }
+
+            result += part;
+        }
+    }
+
+    return result;
 };
--- test.js
+++ test.js
@@ -1,70 +1,74 @@
-var assert = require('assert');
-var chalk = require('chalk');
-var stripAnsi = require('strip-ansi');
-var fixedWidth = require('./');
+const assert = require('assert');
+const chalk = require('chalk');
+const stripAnsi = require('strip-ansi');
+const fixedWidth = require('./');
 
-describe('align', function(){
-  it('should align to left by default', function(){
-    assert(fixedWidth('test', 8) === 'test    ');
-  });
+describe('align', function() {
+    it('should align to left by default', function() {
+        assert(fixedWidth('test', 8) === 'test    ');
+    });
 
-  it('should align to left when wrong align value', function(){
-    assert(fixedWidth('test', 8, { align: 'foo' }) === 'test    ');
-  });
+    it('should align to left when wrong align value', function() {
+        assert(fixedWidth('test', 8, { align: 'foo' }) === 'test    ');
+    });
 
-  it('should align to right when align option is "right"', function(){
-    assert(fixedWidth('test', 8, { align: 'right' }) === '    test');
-  });
+    it('should align to right when align option is "right"', function() {
+        assert(fixedWidth('test', 8, { align: 'right' }) === '    test');
+    });
 });
 
-describe('padding', function(){
-  it('should pad with space by default', function(){
-    assert(fixedWidth('test', 8) === 'test    ');
-  });
+describe('padding', function() {
+    it('should pad with space by default', function() {
+        assert(fixedWidth('test', 8) === 'test    ');
+    });
 
-  it('should pad with specified string', function(){
-    assert(fixedWidth('test', 8, { padding: '.' }) === 'test....');
-  });
+    it('should pad with specified string', function() {
+        assert(fixedWidth('test', 8, { padding: '.' }) === 'test....');
+    });
 });
 
-describe('truncate', function(){
-  it('should truncate with … by default', function(){
-    assert(fixedWidth('hello world', 8) === 'hello w…');
-  });
+describe('truncate', function() {
+    it('should truncate with … by default', function() {
+        assert(fixedWidth('hello world', 8) === 'hello w…');
+    });
 
-  it('should truncate with specified ellipsis string', function(){
-    assert(fixedWidth('hello world', 8, { ellipsis: '...' }) === 'hello...');
-  });
+    it('should truncate with specified ellipsis string', function() {
+        assert(fixedWidth('hello world', 8, { ellipsis: '...' }) === 'hello...');
+    });
 
-  it('should truncate with no ellipsis when passed an empty string', function(){
-    assert(fixedWidth('hello world', 8, { ellipsis: '' }) === 'hello wo');
-  });
+    it('should truncate with no ellipsis when passed an empty string', function() {
+        assert(fixedWidth('hello world', 8, { ellipsis: '' }) === 'hello wo');
+    });
 
-  it('should truncate with no ellipsis when passed a falsy value', function(){
-    assert(fixedWidth('hello world', 8, { ellipsis: false }) === 'hello wo');
-  });
+    it('should truncate with no ellipsis when passed a falsy value', function() {
+        assert(fixedWidth('hello world', 8, { ellipsis: false }) === 'hello wo');
+    });
 });
 
-describe('fit to width colored string', function(){
-  var str = chalk.blue(chalk.green('hello') + '_' + chalk.red('world'));
+describe('fit to width colored string', function() {
+    const str = chalk.blue(chalk.green('hello') + '_' + chalk.red('world'));
 
-  it('default settings', function(){
-    for (var i = 0; i < 12; i++)
-      assert(stripAnsi(fixedWidth(str, i)).length === i);
-  });
+    it('default settings', function() {
+        for (let i = 0; i < 12; i++) {
+            assert(stripAnsi(fixedWidth(str, i)).length === i);
+        }
+    });
 
-  it('truncate left', function(){
-    for (var i = 0; i < 12; i++)
-      assert(stripAnsi(fixedWidth(str, i, { truncate: 'left' })).length === i);
-  });
+    it('truncate left', function() {
+        for (let i = 0; i < 12; i++) {
+            assert(stripAnsi(fixedWidth(str, i, { truncate: 'left' })).length === i);
+        }
+    });
 
-  it('align right', function(){
-    for (var i = 0; i < 12; i++)
-      assert(stripAnsi(fixedWidth(str, i, { align: 'right' })).length === i);
-  });
+    it('align right', function() {
+        for (let i = 0; i < 12; i++) {
+            assert(stripAnsi(fixedWidth(str, i, { align: 'right' })).length === i);
+        }
+    });
 
-  it('align right truncate right', function(){
-    for (var i = 0; i < 12; i++)
-      assert(stripAnsi(fixedWidth(str, i, { align: 'right', truncate: 'right' })).length === i);
-  });
+    it('align right truncate right', function() {
+        for (let i = 0; i < 12; i++) {
+            assert(stripAnsi(fixedWidth(str, i, { align: 'right', truncate: 'right' })).length === i);
+        }
+    });
 });
